2caefdda6d74ddd2373d1005444811d2b7599b75
Alexey Mirniy <alexey.mirniy@gmail.com>
2011-09-14 15:49:26 +0300

    production config was changed.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,10 +17,10 @@ Blog::Application.configure do
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   #config.assets.compile = true
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs
-  config.assets.digest = true
+  config.assets.digest = false
 
   # Defaults to Rails.root.join("public/assets")
   # config.assets.manifest = YOUR_PATH
